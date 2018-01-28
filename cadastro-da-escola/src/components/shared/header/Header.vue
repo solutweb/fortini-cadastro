a14c32d8b5ce80e3955b528a4bcdfb18dc4d2314
Bruno Gomes <bruno@solutweb.com.br>
2018-01-28 17:06:53 -0200

    Added Header component

---
+++ cadastro-da-escola/src/components/shared/header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header" id="header-form">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header-form'
+  }
+</script>
+
+<style scoped>
+  .header {
+    background: #FFF;
+    border: 1px solid #DDD;
+    height: 200px;
+  }
+</style>
