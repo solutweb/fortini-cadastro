f64cf26921394b6317e0f9c2eafa6c9eb2e7ae60
Bruno Gomes <bruno@solutweb.com.br>
2018-01-28 17:08:32 -0200

    Added VueFire setting and Header component

--- cadastro-da-escola/src/main.js
+++ cadastro-da-escola/src/main.js
@@ -1,10 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
+import Header from '@/components/shared/header/Header'
 
 Vue.config.productionTip = false
+Vue.use(VueFire)
+
+Vue.component('header-form', Header)
 
 /* eslint-disable no-new */
 new Vue({
