7b55c6bee19fe7de1f5d3f0c0115e2af3e2f6d6d
Bruno Gomes <bruno@solutweb.com.br>
2018-02-13 21:53:49 -0200

    Style updated and firebase config removed

--- cadastro-da-escola/src/App.vue
+++ cadastro-da-escola/src/App.vue
@@ -6,32 +6,19 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-
-let config = {
-  apiKey: 'AIzaSyA_vU1K13d8EMpahiRPkYguhA3YOeCZcDc',
-  authDomain: 'cadastro-da-escola.firebaseapp.com',
-  databaseURL: 'https://cadastro-da-escola.firebaseio.com',
-  projectId: 'cadastro-da-escola',
-  storageBucket: 'cadastro-da-escola.appspot.com',
-  messagingSenderId: '873838200049'
-}
-
-let app = firebase.initializeApp(config)
-let db = app.database()
 
 export default {
   name: 'App'
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #FFF;
+  width: 90%;
+  margin: 0 auto;
+  padding: 10px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.1);
 }
 </style>
