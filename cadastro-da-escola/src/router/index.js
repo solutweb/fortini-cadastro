40f9449acd7688b76b6e805207e016366d985b06
Bruno Gomes <bruno@solutweb.com.br>
2018-04-20 13:23:26 -0300

    Implement new routes and router security

--- cadastro-da-escola/src/router/index.js
+++ cadastro-da-escola/src/router/index.js
@@ -1,15 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import RegisterForm from '@/components/RegisterForm'
+import LoginForm from '@/components/Login'
+import ViewForm from '@/components/ViewForm'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
+      redirect: '/cadastro'
+    },
+    {
+      path: '/admin',
+      name: 'ViewForm',
+      component: ViewForm,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginForm
+    },
+    {
+      path: '/cadastro',
       name: 'RegisterForm',
       component: RegisterForm
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !currentUser) {
+    next({
+      path: 'login',
+      query: {
+        redirect: to.fullPath
+      }
+    })
+  } else {
+    next()
+  }
+})
+
+export default router
