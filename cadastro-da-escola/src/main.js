1f988a7b97b80a4fadcaa93f54b89882cc0ec61a
Bruno Gomes <bruno@solutweb.com.br>
2018-04-20 13:20:58 -0300

    Import firebase config file

--- cadastro-da-escola/src/main.js
+++ cadastro-da-escola/src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import './firebase'
 import Vue from 'vue'
 import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
 import Header from '@/components/shared/header/Header'
-import '../node_modules/bulma/bulma.sass'
 
 Vue.config.productionTip = false
 Vue.use(VueFire)
@@ -19,3 +19,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+require('../node_modules/bulma/bulma.sass')
